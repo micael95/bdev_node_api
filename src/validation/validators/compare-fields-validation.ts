d3d35bfcab5dbb52d5751131516358cf7ec75537
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-03 23:22:18 -0300

    test: ensure email validators validate

---
+++ src/validation/validators/compare-fields-validation.ts
@@ -0,0 +1,15 @@
+import { Validation } from '@/presenters/protocols'
+import { InvalidParamError } from '@/presenters/errors'
+
+export class CompareFieldsValidation implements Validation {
+  constructor (
+    private readonly fieldName: string,
+    private readonly fieldToCompareName: string
+  ) {}
+
+  validate (input: any): Error {
+    if (input[this.fieldName] !== input[this.fieldToCompareName]) {
+      return new InvalidParamError(this.fieldToCompareName)
+    }
+  }
+}
