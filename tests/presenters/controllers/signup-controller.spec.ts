5a1526d968c8bde1f71576b0456aba91fe00cda6
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-03 22:27:34 -0300

    refactor: add validation params and erros

--- tests/presenters/controllers/signup-controller.spec.ts
+++ tests/presenters/controllers/signup-controller.spec.ts
@@ -1,17 +1,65 @@
+import { Validation } from './../protocols/validation'
+import { AddAccount } from './../../../src/domain/usecases/add-account'
 import { SignUpController } from '@/presenters/controllers/signup-controller'
+import faker from 'faker'
+import { MissingParamError } from '@/presenters/errors'
+import { badRequest } from '@/presenters/helpers'
+import { Authentication } from '@/domain/usecases'
+
+const mockRequest = (): SignUpController.Request => {
+  const password = faker.internet.password()
+  return {
+    name: faker.name.findName(),
+    email: faker.internet.email(),
+    password,
+    passwordConfirmation: password
+  }
+}
+
+class AuthenticationSpy implements Authentication {
+  auth: (authenticationParams: Authentication.Params) => Promise<Authentication.Result>
+}
+
+class AddAccountSpy implements AddAccount {
+  add: (account: AddAccount.Params) => Promise<boolean>
+}
+
+class ValidationSpy implements Validation {
+  error: Error = null
+  input: any
+
+  validate (input: any): Error {
+    this.input = input
+    return this.error
+  }
+}
+
+type SutTypes = {
+  sut: SignUpController
+  addAccountSpy: AddAccountSpy
+  validationSpy: ValidationSpy
+  authenticationSpy: AuthenticationSpy
+}
+
+const makeSut = (): SutTypes => {
+  const authenticationSpy = new AuthenticationSpy()
+  const addAccountSpy = new AddAccountSpy()
+  const validationSpy = new ValidationSpy()
+  const sut = new SignUpController(addAccountSpy, validationSpy, authenticationSpy)
+  return {
+    sut,
+    addAccountSpy,
+    validationSpy,
+    authenticationSpy
+  }
+}
 
 describe('SignUp Controller', () => {
-  test('Should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
-    const httpRequest = {
-      body: {
-        email: 'any_email@gmail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
+  test('Should return 400 if Validation returns an error', async () => {
+    const { sut, validationSpy } = makeSut()
+    validationSpy.error = new MissingParamError(faker.random.word())
+    const httpResponse = await sut.handle(mockRequest())
     expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+    expect(httpResponse).toEqual(badRequest(validationSpy.error))
   })
 })
