b20eab97c0441630a873546c7fe79478249426c1
Micael Duarte <micael.duarte@sinqia.com.br>
2021-07-04 00:22:04 -0300

    refactor: reorganize tests

--- src/main/graphql/resolvers/login.ts
+++ src/main/graphql/resolvers/login.ts
@@ -1,11 +1,7 @@
 import { adaptResolver } from '@/main/adapters'
-import { makeLoginController, makeSignUpController } from '@/main/factories'
+import { makeSignUpController } from '@/main/factories'
 
 export default {
-  Query: {
-    login: async (parent: any, args: any) => adaptResolver(makeLoginController(), args)
-  },
-
   Mutation: {
     signUp: async (parent: any, args: any) => adaptResolver(makeSignUpController(), args)
   }
