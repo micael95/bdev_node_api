68180a469e24a97f8fe1522929eb34583329c5bd
Micael Duarte <micael.duarte@sinqia.com.br>
2021-08-08 13:14:58 -0300

    feat: ensure PasswordController call AddPassword with correct values

--- src/presenters/controllers/add-password-controller.ts
+++ src/presenters/controllers/add-password-controller.ts
@@ -1,6 +1,6 @@
 import { Controller, HttpResponse, Validation } from '@/presenters/protocols'
 import { AddPassword } from '@/domain/usecases/add-password'
-import { noContent, serverError } from '@/presenters/helpers'
+import { badRequest, noContent, serverError } from '@/presenters/helpers'
 
 export class AddPasswordController implements Controller {
   constructor (
@@ -11,6 +11,10 @@ export class AddPasswordController implements Controller {
 
   async handle (request: any): Promise<HttpResponse> {
     try {
+      const error = this.validation.validate(request)
+      if (error) {
+        return badRequest(error)
+      }
       await this.addPassword.add(request)
       return noContent()
     } catch (err) {
